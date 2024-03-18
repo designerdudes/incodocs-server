4e7c0818fd431bf8ff20637ae64eac93177575a7
Mohd Mujahed <mujahedu984@gmail.com>
2024-03-18 09:31:11 +0530

    updated: shipment

---
+++ routes/shipment.js
@@ -0,0 +1,36 @@
+import express from 'express'; 
+import {
+    addShipment,
+    updateShipment,
+    addOrUpdateBookingDetails,
+    addOrUpdateShippingDetails,
+    addOrUpdateShippingBillDetails,
+    addOrUpdateSaleInvoiceDetails,
+    addOrUpdateBlDetails,
+    getAllShipments,
+    getShipmentById,
+    getShipmentsByOrganizationId,
+    deleteShipmentById
+} from '../controllers/shipments.js'; 
+
+
+const router = express.Router();
+
+// Routes for adding, updating, and deleting shipments
+router.post('/add', addShipment);
+router.put('/:id', updateShipment);
+router.delete('/:id', deleteShipmentById);
+
+// Routes for adding or updating shipment details
+router.post('/booking-details', addOrUpdateBookingDetails);
+router.post('/shipping-details', addOrUpdateShippingDetails);
+router.post('/shipping-bill-details', addOrUpdateShippingBillDetails);
+router.post('/sale-invoice-details', addOrUpdateSaleInvoiceDetails);
+router.post('/bl-details', addOrUpdateBlDetails);
+
+// Routes for fetching shipments
+router.get('/getAll', getAllShipments);
+router.get('/get/:id', getShipmentById);
+router.get('/organization/:organizationId', getShipmentsByOrganizationId);
+
+export default router;
