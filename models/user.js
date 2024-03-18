56f14010361767b89888998be928cb5cd9b3f363
Mohd Mujahed <mujahedu984@gmail.com>
2024-03-19 04:29:18 +0530

    updated   : user

--- models/user.js
+++ models/user.js
@@ -31,7 +31,11 @@ const userSchema = new mongoose.Schema({
         type: Number,
         sparse: true,
     },
-    address: [addressSchema],
+    address: addressSchema,
+    password:{
+        type: String,
+        required:true,
+    },
     profileImg: {
         type: String,
         default: 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png',
