bbb1702a87e707311826fd4d388586ab404cf174
mohammed ahmed khan <84978114+mak0551@users.noreply.github.com>
2024-11-28 19:05:26 +0530

    suppplier APIs

---
+++ models/accounting/suppliers&customers.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+// Supplier collection
+const supplierSchema = new mongoose.Schema(
+  {
+    supplierName: { type: String },
+    gstNo: { type: String },
+    mobileNumber: { type: Number },
+    state: { type: String },
+    factoryAddress: { type: String },
+  },
+  { timestamps: true }
+);
+
+export const supplier = mongoose.model("supplier", supplierSchema);
+
+// Customer collection
+const customerSchema = new mongoose.Schema(
+  {
+    customerName: { type: String },
+    gstNo: { type: String },
+    mobileNumber: { type: Number },
+    state: { type: String },
+    address: { type: string },
+  },
+  { timestamps: true }
+);
+
+export const customer = mongoose.model("customer", customerSchema);
