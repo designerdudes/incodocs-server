5e7fc2a565318b7548355c216a7687cc4312be9c
mohammed ahmed khan <84978114+mak0551@users.noreply.github.com>
2025-02-12 19:49:53 +0530

    shipment schema done

---
+++ models/documentation/consignee&productDetails.js
@@ -0,0 +1,54 @@
+import mongoose from "mongoose";
+
+const tileSchema = new mongoose.Schema({
+  noOfBoxes: Number,
+  noOfPiecesPerBoxes: Number,
+  sizePerTile: {
+    length: {
+      value: { type: Number },
+      units: { type: String, enum: ["inch", "cm"] },
+    },
+    breadth: {
+      value: { type: Number },
+      units: { type: String, enum: ["inch", "cm"] },
+    },
+  },
+});
+
+const slabSchema = new mongoose.Schema({
+  noOfBundles: Number,
+  noOfSlabsPerBundle: Number,
+  uploadMeasurementSheetUrl: String,
+  totalSQMTRorSQFTwithAllowance: String,
+  totalSQMTRorSMFTwithoutAllowance: String,
+});
+
+const productDetailsSchema = new mongoose.Schema(
+  {
+    productCategory: { type: String, enum: ["Granite & marble", "Ceramic"] },
+    graniteAndMarble: { type: String, enum: ["tiles", "Slabs"] },
+    tiles: tileSchema,
+    slabs: slabSchema,
+  },
+  { timestamps: true }
+);
+
+const productDetails = mongoose.model("productDetails", productDetailsSchema);
+
+const consigneeSchema = new mongoose.Schema(
+  {
+    name: String,
+    address: String,
+    TelephoneNo: Number,
+    email: String,
+    organizationId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Organization",
+    },
+  },
+  { timestamps: true }
+);
+
+const consignee = mongoose.model("consignee", consigneeSchema);
+
+export { consignee, productDetails };
--- models/documentation/shipment.js
+++ models/documentation/shipment.js
@@ -1,18 +1,27 @@
 import mongoose from "mongoose";
 
+const containersSchema = new mongoose.Schema({
+  containerNumber: { type: Number },
+  truckNumber: { type: String },
+  trukDriverContactNumber: { type: Number },
+  addProductDetails: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "productDetails",
+  },
+});
+
 const bookingDetailsSchema = new mongoose.Schema({
-  containerNumber: [String],
+  bookingNumber: String,
   portOfLoading: String,
   destinationPort: String,
   vesselSailingDate: Date,
   vesselArrivingDate: Date,
-  truckNumber: String,
-  truckDriverNumber: String,
+  numberOfContainers: [containersSchema],
 });
 
 const shippingDetailsSchema = new mongoose.Schema({
   shippingLine: String,
-  forwarder: String,
+  forwarderName: String,
   forwarderInvoice: String,
   valueOfForwarderInvoice: String,
   transporter: String,
@@ -20,39 +29,68 @@ const shippingDetailsSchema = new mongoose.Schema({
   valueOfTransporterInvoice: String,
 });
 
+const shippingBillSchema = new mongoose.Schema({
+  shippingBillUrl: { type: String },
+  shippingBillNumber: { type: String },
+  shippingBillDate: { type: Date },
+  drawbackValue: { type: String },
+  roadtepValue: { type: String },
+});
+
 const shippingBillDetailsSchema = new mongoose.Schema({
-  shippingBillNumber: String,
-  shippingBillDate: Date,
-  uploadShippingBill: String,
-  cbName: String,
+  portCode: { type: String },
+  cbName: { type: String },
+  cdCode: { type: String },
+  ShippingBills: [shippingBillSchema],
+});
+
+const clearanceSchema = new mongoose.Schema({
+  supplierName: String,
+  supplierGSTN: String,
+  supplierInvoiceNumber: String,
+  supplierInvoiceDate: Date,
+  supplierInvoiceValueWithGST: String,
+  supplierInvoiceValueWithOutGST: String,
+  clearanceSupplierInvoiceUrl: String,
+});
+
+const actualSchema = new mongoose.Schema({
+  actualSupplierName: String,
+  actualSupplierInvoiceValue: String,
+  actualSupplierInvoiceUrl: String,
+  shippingBillUrl: String,
 });
 
 const supplierDetailsSchema = new mongoose.Schema({
-  supplierName: String,
-  actualSupplierName: String,
-  supplierGSTIN: String,
-  supplierInvoiceNumber: String,
-  supplierInvoiceDate: Date,
-  supplierInvoiceValueWithOutGST: String,
-  supplierInvoiceValueWithGST: String,
-  uploadSupplierInvoice: String,
-  actualSupplierInvoice: String,
-  actualSupplierInvoiceValue: String,
+  clearance: clearanceSchema,
+  actual: actualSchema,
+});
+
+const commercialInvoiceSchema = new mongoose.Schema({
+  commercialInvoiceNumber: String,
+  clearanceCommercialInvoiceUrl: String,
+  actualCommercialInvoiceUrl: String,
+  saberInvoiceUrl: String,
 });
 
 const saleInvoiceDetailsSchema = new mongoose.Schema({
-  uploadCommercialInvoice: String,
-  commercialInvoiceNumber: String,
-  commercialInvoiceDate: Date,
-  consigneeDetails: String,
+  consignee: { type: mongoose.Schema.Types.ObjectId, ref: "consignee" },
   actualBuyer: String,
+  commercialInvoices: commercialInvoiceSchema,
 });
 
 const blDetailsSchema = new mongoose.Schema({
   blNumber: String,
   blDate: Date,
   telexDate: Date,
-  uploadBL: String,
+  uploadBLUrl: String,
+});
+
+const certificateOfOriginSchema = new mongoose.Schema({
+  certificateOfOriginNumber: String,
+  date: Date,
+  issuerOfCOO: String,
+  uploadCopyOfFumigationCertificate: String,
 });
 
 const shipmentSchema = new mongoose.Schema(
@@ -63,6 +101,7 @@ const shipmentSchema = new mongoose.Schema(
     supplierDetails: supplierDetailsSchema,
     saleInvoiceDetails: saleInvoiceDetailsSchema,
     blDetails: blDetailsSchema,
+    certificateOfOrigin: certificateOfOriginSchema,
     organization: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "Organization",
